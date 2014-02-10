2dff5c48d56fc94b7e052884e6b798f48938afdd
blancgr <gregory.blanc@iut-valence.fr>
2014-02-10 17:32:14 +0100

    evolution 1

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,8 +2,9 @@ package test;
 
 public class Test
 {
-    /*
+    /**
      * Test
+     * @author blancgr
      */
     public static void main(String[]args); // methode principal
     Lampe maLampe = new Lampe // création d'une lampe
